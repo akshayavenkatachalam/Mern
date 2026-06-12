51821b09ad5dc3ec6ae6fbe4602933d84f00b49a
akshayavenkatachalam <akshayav25csa@vetias.ac.in>
2026-06-12 11:59:17 +0530

    comparison

--- comparison.js
+++ comparison.js
@@ -1,2 +1,16 @@
 //comparison
 console.log("comparison operator")
+var a=10
+var b=30
+console.log(a==b)//equal
+console.log(a!=b)//not equal
+console.log(a<=b)
+console.log(a>=b)
+console.log(a<b)
+console.log(a>b)
+console.log(a===b)
+
+
+
+
+
