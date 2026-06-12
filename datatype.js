20b420b214473ad2d18c052a533bb2577d2785d8
akshayavenkatachalam <akshayav25csa@vetias.ac.in>
2026-06-12 15:31:34 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,37 @@
+console.log("welcome to primitive data types")
+
+
+//numbers
+console.log("DT Numbers")
+var age = 18
+console.log("age")
+
+//strings
+console.log("DT Strings")
+var name ="sachin"
+console.log(name)
+
+//booleans
+console.log("DT Boolean")
+var  isstudent = false
+console.log(isstudent)
+
+//null
+console.log("DT Null")
+var b =null
+console.log(null)
+
+//undefined
+console.log("DT Undefined")
+var c
+console.log(c)
+
+//bigint
+console.log("DT bigint")
+var d=123450123450123450123450123450
+console.log(d)
+
+//symbol
+var e= Symbol ("symbol")
+console.log(e)
+
--- operator.js
+++ operator.js
@@ -6,3 +6,42 @@ console.log(a+=10)
 console.log(a-=10)
 console.log(a*=10)
 console.log(a/=10) 
+
+
+//comparison
+console.log("comparison operator")
+var a=10
+var b=30
+console.log(a==b)//equal
+console.log(a!=b)//not equal
+console.log(a<=b)
+console.log(a>=b)
+console.log(a<b)
+console.log(a>b)
+console.log(a===b)
+
+
+console.log("logical operator")
+var a=10
+var b=30
+console.log(a>0 && b>0)//and true
+console.log(a<0 && b<0)//and false
+console.log(a>0 ||  b>0)//or true
+console.log(!a>0)//logical not false
+
+console.log('unary operator')
+var c=10
+console.log(c++)// 10 c - 11
+console.log(c--)// 11 c - 10 
+console.log(++c)// 11 c - 11
+console.log(--c)// 10 c -10
+
+console.log("ternary operator")
+var a= 10
+var b= 20
+var result =(a<b)? "a is less":"b is less"
+//var variable name =(condition)?l 56385
+console.log(result)
+
+
+
