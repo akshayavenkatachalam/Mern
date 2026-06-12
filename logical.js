d9ccefb5bd3565e5002c0de5233b00656c8937e9
akshayavenkatachalam <akshayav25csa@vetias.ac.in>
2026-06-12 12:10:05 +0530

    logical

---
+++ logical.js
@@ -0,0 +1,7 @@
+console.log("logical operator")
+var a=10
+var b=30
+console.log(a>0 && b>0)//and true
+console.log(a<0 && b<0)//and false
+console.log(a>0 ||  b>0)//or true
+console.log(!a>0)//logical not false
