23dcff729bdb8920d6ec829b05fc9798775d1552
akshayavenkatachalam <akshayav25csa@vetias.ac.in>
2026-06-12 10:40:37 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+//variable declaration using var
+var a='30'
+console.log(a)
+//variable declaration using let
+let name='akshaya'
+console.log(name)
+// variable using const
+const country='india'
+console.log(country)
