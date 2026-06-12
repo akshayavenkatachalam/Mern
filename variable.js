2bed67f8a9c6a8ab992c331c529dd09c2bc7a6da
akshayavenkatachalam <akshayav25csa@vetias.ac.in>
2026-06-12 11:24:26 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,10 @@
+//arithmetic operators
+
+var a=50;
+var b=25;
+console.log(a+b)//addition
+console.log(a-b)//subraction
+console.log(a*b)//product
+console.log(a%b)//mod
+console.log(a**b)//expo
+
